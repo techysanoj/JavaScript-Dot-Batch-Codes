0bee11732e06c95987c93a6a549186ca43caa115
Sanoj <96434101+techysanoj@users.noreply.github.com>
2024-10-17 21:11:41 +0530

    Adding 3-JS-Basics code + notes

--- JS Basics/JS_BASICS_3/script.js
+++ JS Basics/JS_BASICS_3/script.js
@@ -1,174 +1,245 @@
-console.log('JS Basics - 2');
-
-const rectangle = {
-    length: 1,
-    breadth: 2,
-    draw: function() {
-        console.log('Area is : ' + this.length * this.breadth);
-    }
-};
-
-console.log(rectangle); 
-console.log(rectangle.draw); // it will print the brackets and all
-console.log(rectangle.draw()); // now it will print Area is : 2
+// Math - inbuilt Object
+console.log(Math.random());
+console.log(Math.PI);
+console.log(Math.round(1.56));
+console.log(Math.max(2, 3, 4, 5, 10, 1, 2));
+console.log(Math.abs(-2)); 
 
 
-// Factory Function
+// String - inbuilt Object (String are also primitive and also object)
+console.log('Hello');
+console.log(typeof('Hello')); // it will output string
 
-// function createRectange() {
-//     let rectangle = {
-//         length: 1,
-//         breadth: 2,
-//         // draw: function() {
-//         //     console.log('Area is : ' + this.length * this.breadth);
-//         // }
-//         draw() {
-//             console.log('Area is : ' + this.length * this.breadth);
-//         }
-//     };
-//     return rectangle;
-// }
-function createRectange(length, breadth) {
-    let rectangle = {
-        length: length,
-        breadth: breadth,
-        // draw: function() {
-        //     console.log('Area is : ' + this.length * this.breadth);
-        // }
-        draw() {
-            console.log('Area is : ' + this.length * this.breadth);
-        }
-    };
-    return rectangle;
+let lastName = 'Kumar';
+let firstName = new String('Sanoj'); // it will create an Object
+
+console.log(typeof(lastName) + " ::: " + typeof(firstName));
+// but we can create string as Object by using . (dot operator)
+console.log(lastName.length); // 5
+console.log(lastName[0]); // K
+console.log(lastName.includes('Kum')); // true
+console.log(lastName.startsWith('K')); // true
+console.log(lastName.indexOf('r')); // 4
+console.log(lastName.toUpperCase()); // converts in uppercase
+console.log(lastName.trim());
+console.log(lastName.replace('Kum', 'Son')); // Sonar
+
+let message = 'This is my message';
+let words = message.split(' ');
+console.log(words); // it will give me arrays of string spliting the message acc to ' '.
+
+
+// Template Literal
+let message1 = 'Sanoj \n is a good \n boy';
+console.log(message1);
+
+let message2 = `
+    Sanoj is
+    a good boy.
+    "Hello boy".
+    'Giving you brave heart'
+`;
+console.log(message2);
+
+let message3 = `
+ my name is ${lastName}
+`;
+console.log(message3);
+
+
+// Date and Time Literal
+let dateN = new Date();
+console.log(dateN); // it will print complete current date and time
+
+let date2 = new Date('June 20 1998 07:15');
+console.log(date2);
+
+let date3 = new Date(1998, 11, 20, 7); // month indexing 0 to 11.
+console.log(date3); // year month date hour
+
+date3.setFullYear(2002);
+console.log(date3);
+
+
+// Arrays
+let numbers = [1, 3, 4, 5];
+console.log(numbers);
+// Insertion in the arrays
+numbers.push(9); // insertion at the end
+numbers.unshift(8); // insertion at the first
+numbers.splice(2, 0, '0', '0', '0'); // insert at the middle
+console.log(numbers);
+
+// Searcing in the arrays
+console.log(numbers.indexOf(4)); // prints 2
+console.log(numbers.indexOf(100)); // prints -1 because not present
+
+console.log(numbers.includes(9)); // return true
+console.log(numbers.includes(100)); // return false;
+
+console.log(numbers.indexOf(7, 3)); // start searching with the starting index 3
+
+// searching in the arrays of objects
+let courses = [
+    {no:1, naam: 'Love'}, 
+    {no: 2, naam: 'Sanoj'},
+    {no:3, naam: 'Love'}
+];
+console.log(courses);
+console.log(courses.indexOf({no:1, naam:'Love'})); // it will give -1 but it is present.
+// why? - here reference matters reference of given values are different 
+
+// Here we will use callback functions
+// in find(predicate function) - predicate function as parameters 
+// and return the object.
+let course = courses.find(function (courses) {
+    return courses.naam == 'Love'; // it will return the first object whose naam is 'Love'.
+})
+console.log(course); // course will be an object.
+
+// using arrow function to reduce the lines () => {}
+let course2 = courses.find((courses)=>{
+    return courses.naam == 'Sanoj';
+})
+console.log(course2);
+
+
+// Removing Elements
+let num = [1, 2, 3, 4, 5];
+num.pop();
+console.log(num); // 1, 2, 3, 4
+
+num.shift();
+console.log(num); // 2, 3, 4
+
+num.splice(1, 2); // it will delete 2 elements starting with index 1
+console.log(num);
+
+
+// Emptying an array
+let num1 = [1, 2, 3, 4, 5];
+let num2 = num1;
+// num1 = [];
+// console.log(num1); // here it will print empty
+// console.log(num2); // here it will give 1, 2, 3, 4, 5
+
+// num1.length = 0;
+// console.log(num1); // now it will be empty
+// console.log(num2); // it will also be empty
+
+num1.splice(0, num.length);
+console.log(num1); // it will be empty
+console.log(num2);  // it will also be empty
+
+// using for loop we can do pop() operation
+
+
+// Combining the arrays
+let first = [1, 2, 3];
+let second = [4, 5, 6];
+let combined = first.concat(second);
+console.log(combined); // 1, 2, 3, 4, 5, 6
+
+
+// Slicing the array
+let sliced = combined.slice(2, 3);
+console.log(sliced); // here 3 only because 3 index is not include means [low, end);
+let sliced2 = combined.slice(2); // it will give all the elemtns starting from index 2
+let sliced3 = combined.slice(); // it will create a copy of the array
+
+// Slicing the objects
+let obj = { a: 1, b: 2, c: 3, d: 4 };
+let keys = ['a', 'c'];
+// Slicing object properties
+let slicedObj = Object.fromEntries(
+    Object.entries(obj).filter(([key]) => keys.includes(key))
+);
+console.log(slicedObj); // { a: 1, c: 3 }
+
+
+// Spread Operator
+let f = [1, 2, 3];
+let s = [4, 5, 6];
+let com = [...f, ...s];
+console.log(com); // 1, 2, 3, 4, 5, 6
+com = [...f, 'a', 'b', ...s];
+console.log(com); // 1, 2, 3, a, b, 4, 5, 6
+
+
+// Iterating the arrays, traversing
+// For of Loop
+for(let val of com) {
+    console.log(val);
 }
-// now we will call this funciton and change the values
-let rectangle1 = createRectange(3, 5);
-console.log(rectangle1.draw());
+// it will print 1, 2, 3, a, b, 4, 5, 6
 
-// to check if any property is present in it or not
-if ('color' in rectangle1) {
-    console.log('Yes present');
-}
-else {
-    console.log('Not present');
-}
-// it will print Not Present
+// For each Loop - it requires a function as a parameter
+// can be arrow function or can be  normal function
+com.forEach((number) => {
+    console.log(number);
+});
 
 
-
-// Constructor Function
-
-function Rectangle(len, brea) {
-    this.length = len;
-    this.breadth = brea;
-    this.draw = function() {
-        console.log('Area is : ' + this.length * this.breadth);
-    }
-}
-// here only initiallization of the variable are done no necessary to return it
-let rec1 = new Rectangle(5, 5);
-console.log(rec1.draw());
-
-rec1.height = 10;
-console.log(rec1);
-rec1.collor = 'Black';
-console.log(rec1);
-
-delete rec1.height;
-console.log(rec1);
-
-console.log(rec1.constructor); // it will print Rectangle Function
-console.log(rec1.constructor.constructor); // it will print inbuilt Constructor Function
+// Joining the arrays
+let numArr = [10, 20, 30, 40, 50];
+const joined = numArr.join(',');
+console.log(joined); // 10, 20, 30, 40, 50;
+// it will print in single line beacuse it is now joined using ,
 
 
-let Rectangle1 = new Function(
-    'length', 'breadth',
-    `this.length = length;
-    this.breadth = breadth;
-    this.draw = function() {
-        console.log('Area is : ' + this.length * this.breadth);
-    }`);
-
-let rect = new Rectangle1(4, 3);
-console.log(rect.draw());
-
-console.log(Rectangle1.length);
+// Spliting the arrays
+let msg = 'This is my first message';
+let parts = message.split(' '); // it will split acc to ' '.
+// and it returns the array
+console.log(parts);
 
 
-let check1 = {
-    a: 10
-};
+// Sorting the arrays
+let numArray = [10, 1, 2, 11, -1, 40];
+numArray.sort();
+console.log(numArray); // -1, 1, 2, 10, 11, 40;
 
-let check2 = check1;
-++check2.a;
-console.log(check1.a + " " + check2.a); // it will print 11 11
+numArray.reverse(); // reverse the array
 
-let a1 = 10;
-let a2 = a1;
-++a1;
-console.log(a1 + " " + a2); // it will print 11 10
-
-let a3 = 10;
-function inc(a) {
-    a++;
-}
-inc(a3);
-console.log(a3); // it will print 10 not 11 
-//because copy is passed not the reference
-
-let a4 = {
-    value: 10
-};
-
-function inc(a) {
-    ++a.value;
-}
-inc(a4);
-console.log(a4.value); // here value is 11 because address is same
+let strArr = ['b', 'a', 'd', 'f', 'c'];
+strArr.sort();
+console.log(strArr);
 
 
+// Filtering the arrays.
+let arrNum = [1, 2, -1, -4, 3, 5];
+let posNum = arrNum.filter(function(values) {
+        return values >= 0;
+    });
+console.log(posNum); // 1, 2, 3, 5
 
-// For in Loop
-for(let key in rectangle1) {
-    console.log(key);
-}
-// it is used to iterate to all keys present in that object
-
-
-// For of loop
-let arr = [1, 2, 3, 4, 5];
-for(let el of arr) {
-    console.log(el);
-}
-
-// we have converted rectangle1 object into arrays of its keys
-for(let key of Object.keys(rectangle1)) {
-    console.log(key);
-}
-
-// we have converted rectangle1 object into arrays of its entries
-for(let key of Object.entries(rectangle1)) {
-    console.log(key);
-}
+let negNum = arrNum.filter((val) => {
+    return val < 0;
+});
+console.log(negNum); // -1, -4;
 
 
+// Mapping the arrays
+let arrNum1 = [1, 2, 3, 4, 5];
+let items = arrNum1.map(function(value) {
+    return 'stu_no' + value;
+});
+console.log(items); // stu_no1, stu_no2, and so on
 
-// Object Cloning 
 
-// Iteration
-let src = {
-    value: 10,
-    firstName: 'Src1'
-};
-let dest = {};
-for(let key in src) {
-    dest[key] = src[key];
-}
-console.log(dest); // it will print same as src but not same address
+// Mapping with objects
+let numberArr = [1, 2, -6, -9];
+let filtered = numberArr.filter(value => value>=0);
 
-//Assign
-let dest2 = Object.assign({}, src);
-console.log(dest2);
+let itemsNew = filtered.map(function(value) {
+    return {value : num};
+});
+console.log(itemsNew);
 
-// Spread
-let dest3 = {...src};
+// here we can use function chaining also
+let itemsNew1 = numberArr
+                .filter((value) => value >= 0)
+                .map(function(value) {
+                    return {value : num};
+                });
+console.log(itemsNew1);
